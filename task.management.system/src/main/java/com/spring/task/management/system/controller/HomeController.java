192b86052bf39ca2ed48e86d6a3986ae13a38d74
Bella Sabeva <BSabeva@technologica.com>
2022-02-11 22:30:04 +0200

    login.html

---
+++ task.management.system/src/main/java/com/spring/task/management/system/controller/HomeController.java
@@ -0,0 +1,28 @@
+package com.spring.task.management.system.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.security.Principal;
+
+
+@Controller
+class HomeController {
+
+   @ModelAttribute("module")
+   String module() {
+      return "home";
+   }
+
+   @GetMapping("/")
+   String index(Principal principal) {
+      return principal != null ? "homeSignedIn" : "homeNotSignedIn";
+   }
+}
+
+
+
+
+
+
---
+++ task.management.system/src/main/java/com/spring/task/management/system/controller/SigninController.java
@@ -0,0 +1,12 @@
+package com.spring.task.management.system.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SigninController {
+    @RequestMapping(value = "login")
+    public String signin() {
+        return "login";
+    }
+}
